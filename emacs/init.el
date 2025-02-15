52049bda7c81650afc32349422020ead8f199eff
Michael van Rooijen <michael@vanrooijen.io>
2025-02-15 13:26:54 +0100

    [Emacs] Add Aider

--- emacs/init.el
+++ emacs/init.el
@@ -598,4 +598,16 @@
 
 
 
+;; Aider
 
+(use-package aider
+  :straight (:host github :repo "tninja/aider.el" :files ("aider.el"))
+  :general (:states '(normal) "§ a" 'aider-transient-menu)
+  :config
+  (setenv "OPENAI_API_KEY" (read-op-item "op://Shared/OpenAI/emacs-key"))
+  (setenv "GEMINI_API_KEY" (read-op-item "op://Private/Google/emacs-key"))
+  (setq aider-args '("--model" "gemini/gemini-2.0-flash" "--weak-model" "gemini/gemini-2.0-flash")))
+
+(eval-after-load 'aider
+  '(setq aider-popular-models
+         '("o3-mini" "gemini/gemini-2.0-flash")))
