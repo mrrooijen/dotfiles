a5ddd7df737f8f278650e27a75d91a200c51e148
Michael van Rooijen <michael@vanrooijen.io>
2025-07-01 11:14:54 +0200

    [Emacs] Remove custom formatting for Python

--- emacs/init/python.el
+++ emacs/init/python.el
@@ -1,13 +1,4 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package python
-  :mode ("\\.py\\'" . python-mode)
-  :init
-  (setq-default indent-tabs-mode nil
-                tab-width 4
-                python-indent-offset 4)
-  :hook
-  (python-mode . (lambda ()
-                   (setq indent-tabs-mode nil
-                         tab-width 4
-                         python-indent-offset 4))))
+  :mode ("\\.py\\'" . python-mode))
