c8f324d9621513cc93983466b618ab66e8832ebe
Michael van Rooijen <michael@vanrooijen.io>
2025-07-09 19:14:57 +0200

    WIP

--- emacs/init/notes.el
+++ emacs/init/notes.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
-(setq remember-data-file "~/Documents/Notes/emacs")
+(setq remember-data-file "~/Documents/Notes/emacs.md")
 (setq initial-buffer-choice 'remember-notes)
 (add-hook 'emacs-startup-hook (lambda () (when (string= (buffer-name) "*notes*") (markdown-mode))))
 (advice-add 'remember-notes :after (lambda (&rest _) (markdown-mode)))
