3a5724e7a24a2dbcbd453c704e2658386181652d
Michael van Rooijen <michael@vanrooijen.io>
2025-07-13 14:49:01 +0200

    WIP

--- emacs/init/keybindings.el
+++ emacs/init/keybindings.el
@@ -1,5 +1,26 @@
 ;; -*- lexical-binding: t; -*-
 
+(use-package general
+  :straight t
+  :config
+  (general-auto-unbind-keys)
+  (general-define-key
+   "s-w"     'delete-window
+   "M-f"     'delete-other-windows
+
+   "s-B"     'previous-buffer
+   "s-k"     'kill-this-buffer-unless-scratch
+   "s-K"     'kill-other-buffers
+
+   "s-q"     nil
+   "§ q q"   'delete-frame
+   "§ q k"   'kill-emacs
+   "§ q r"   'restart-emacs
+   "§ l"     'cycle-language
+   "§ L"     'flyspell-buffer)
+  (general-define-key :keymaps 'override "M-<tab>" 'other-window)
+  (define-key key-translation-map (kbd "M-<tab>") (kbd "C-x o")))
+
 (use-package which-key
   :straight t
   :config
@@ -7,28 +28,3 @@
   (which-key-add-key-based-replacements
     "§ f" "find"
     "§ q" "quit"))
-
-(use-package general
-  :straight t
-  :config
-  (general-auto-unbind-keys))
-
-(general-define-key
- "s-w"     'delete-window
- "M-f"     'delete-other-windows
-
- "s-B"     'previous-buffer
- "s-k"     'kill-this-buffer-unless-scratch
- "s-K"     'kill-other-buffers
-
- "s-q"     nil
- "§ q q"   'delete-frame
- "§ q k"   'kill-emacs
- "§ q r"   'restart-emacs
- "§ l"     'cycle-language
- "§ L"     'flyspell-buffer)
-
-;; Force M-<tab> to other-window using a combination of
-;; global-set-key and key-translation-map
-(general-define-key :keymaps 'override "M-<tab>" 'other-window)
-(define-key key-translation-map (kbd "M-<tab>") (kbd "C-x o"))
